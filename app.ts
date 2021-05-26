5279213dc83cdced7a140b2ceb846028664b0ef1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-05-26 15:35:09 -0400

    fixed errors in app.ts

--- app.ts
+++ app.ts
@@ -4,11 +4,11 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
-export default app;
+const app = express();
+export default app; // exports app as the default Object for this module
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,12 +24,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) 
+{
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err:createError.HttpError, req:express.Request, res:express.Response, next: express.NextFunction) 
+{
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
